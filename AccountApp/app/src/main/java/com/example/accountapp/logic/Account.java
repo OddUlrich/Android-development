5812b59ce70b1d5ce2040f5bc097825460c00c8a
OddUlrich <vmoonodd@gmail.com>
2019-09-19 19:12:25 +1000

    Finished accounting.

---
+++ AccountApp/app/src/main/java/com/example/accountapp/logic/Account.java
@@ -0,0 +1,99 @@
+package com.example.accountapp.logic;
+
+import com.example.accountapp.ui.OutputInterface;
+
+/**
+ * This file defines the Account class.  It provides the basis for a
+ * series of improvements you'll need to make as you progress through
+ * the lessons in Module 6.
+ */
+public class Account {
+    /**
+     * This is the variable that stores our OutputInterface instance.
+     * <p/>
+     * This is how we will interact with the User Interface
+     * [MainActivity.java].
+     * </p>
+     * This was renamed to 'mOut' from 'out', as it is in the video
+     * lessons, to better match Android/Java naming guidelines.
+     */
+    final OutputInterface mOut;
+
+    /**
+     * Name of the account holder.
+     */
+    String name;
+
+    /**
+     * Number of the account.
+     */
+    int number;
+
+    /**
+     * Current balance in the account.
+     */
+    double balance;
+
+    /**
+     * Constructor initializes the field
+     */
+    public Account(OutputInterface out) {
+        mOut = out;
+    }
+    public Account(String newName, int newNumber) {
+        this.name = newName;
+        this.number = newNumber;
+    }
+    public Account(String newName, int newNumber, double initialBalance) {
+        this(newName, newNumber);
+        this.balance = initialBalance;
+    }
+
+    /**
+     * Deposit @a amount into the account.
+     */
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    /**
+     * Withdraw @a amount from the account.  Prints "Insufficient
+     * Funds" if there's not enough money in the account.
+     */
+    public void withdrawal(double amount) {
+        if (balance > amount)
+            balance -= amount;
+        else 
+            mOut.println("Insufficient Funds");
+    }
+
+    /**
+     * Display the current @a amount in the account.
+     */
+    public void displayBalance() {
+        mOut.println("The balance on account " 
+                     + number
+                     + " is " 
+                     + balance);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
+    }
+
+    public void setNumber(int newNum) {
+        this.number = newNum;
+    }
+}
